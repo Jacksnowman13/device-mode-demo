8fcd43d36ef154836c626b9743b9b583c3c5e0e9
Ben Zhou <benzhouprivate@gmail.com>
2026-08-05 14:13:57 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "device-mode-demo-v1";
+const CACHE = "device-mode-demo-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
@@ -7,7 +7,11 @@ self.addEventListener("install", event => {
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+      caches.keys()
+          .then(keys => Promise.all(
+              keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+          .then(() => self.clients.claim()));
 });
 
 self.addEventListener("fetch", event => {
